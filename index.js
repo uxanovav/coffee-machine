11d008770d5a787f31a32a938f0b7c9617669ce0
uxanovav <uxanovav@yandex.ru>
2020-11-13 23:28:58 +0400

    feat: inital commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.button');
+const screen = document.querySelector('.screen');
+const cancelButton = document.querySelector('.cancel-button');
+const cup = document.querySelector('.cup');
+
+let state = {
+    middleCupCount: 5,
+    bigCupCount = 6,
+    
+}
