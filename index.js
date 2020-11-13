9096a7e50ca0d71adaa64020bb389f22a5df3543
uxanovav <uxanovav@yandex.ru>
2020-11-14 00:34:29 +0400

    feat: add some options

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const buttons = document.querySelectorAll('.button');
+import {options} from "./options.js";
+const mainDrinkButtons = document.querySelectorAll('.main-drink');
+const additionalDrinkButtons = document.querySelectorAll('.additional-drink')
 const screen = document.querySelector('.screen');
 const cancelButton = document.querySelector('.cancel-button');
 const cup = document.querySelector('.cup');
 
-let state = {
-    middleCupCount: 5,
-    bigCupCount = 6,
-    
+let drink = {
+    mainDrink: '',
+    optionalDrink: '',
+    selectedCup: '',
+    totalVolume: 0,
+    totalPrice: 0
+}
+
+cancelButton.addEventListener('click', resetDrink)
+
+
+function resetDrink() {
+    drink.mainDrink = '';
+    drink.optionalDrink = '';
+    drink.selectedCup ='',
+    drink.totalVolume = 0,
+    drink.totalPrice = 0
+}
+
+mainDrinkButtons.forEach(element => {
+    element.addEventListener('click', addMainDrink);
+});
+
+function addMainDrink() {
+        let keyName = this.dataset.key;
+        drink.mainDrink = this.dataset.key;
+        drink.totalVolume = options[`${keyName}`].volume;
+        drink.totalPrice = options[`${keyName}`].price;
+        console.log(drink);
 }
---
+++ options.js
@@ -0,0 +1,42 @@
+export const options = {
+    middlecup: {
+        count: 5,
+        value: 250
+    },
+    bigcup: {
+        count: 6,
+        value: 350
+    },
+    espresso: {
+        volume: 100,
+        price: 90,
+    },
+    latte: {
+        volume:250,
+        price:130
+    },
+    сappuccino: {
+        volume:250,
+        price:110
+    },
+    bananalatte: {
+        volume:300,
+        price:150
+    },
+    vanillacappuccino: {
+        volume:300,
+        price:150
+    },
+    flatwhite: {
+        volume:280,
+        price:100
+    },
+    milk: {
+        volume:50,
+        price:25
+    },
+    syrop: {
+        volume:50,
+        price:35
+    }
+}
